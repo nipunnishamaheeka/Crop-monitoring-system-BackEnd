4a6d47d4a0336de9a8bdd6a4785409c30143750a
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-10-30 22:36:53 +0530

    Frist commit

---
+++ cropsMonitoring/src/test/java/lk/ijse/cropsMonitoring/CropsMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cropsMonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropsMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ca8c7f57ca53064bcc9ff08b5d384d8fe6fd4abd
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-11-20 22:57:15 +0530

    updated

--- cropsMonitoring/src/main/java/lk/ijse/cropsMonitoring/dao/MonitoringLogServiceDAO.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.cropsMonitoring.dao;
-
-import lk.ijse.cropsMonitoring.entity.CropDetailsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MonitoringLogServiceDAO extends JpaRepository<CropDetailsEntity,String> {
-}
